3d441189a9d6ca90dbdd9c2f5563c4d6498bbbd5
Sungho Park <chywoo.park@samsung.com>
2015-03-03 11:38:37 +0900

    HFJS 2007 chapter01

--- test_windows.js
+++ test_windows.js
@@ -6,8 +6,8 @@ function body_load()
 {
     console.log("Start function");
 
-    windows.confirm("Move windows")
+    windows.confirm("Move windows");
     windows.moveBy(10, 10);
 
-    return;
+
 }
